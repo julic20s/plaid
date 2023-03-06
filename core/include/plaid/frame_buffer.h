882b4fae952e8232628226dda75d75d27ff25344
julic20s <julic20s@outlook.com>
2023-03-06 15:33:31 +0800

    Add comments

--- core/include/plaid/frame_buffer.h
+++ core/include/plaid/frame_buffer.h
@@ -6,13 +6,21 @@
 namespace plaid {
 
 /// 记录一个帧缓冲区，一个帧缓冲区可以包含多个附件
+/// 它会持有全部附件内存的指针，但不具有所有权
 struct frame_buffer {
 public:
 
+  /// 附件内存指针
   using attachment = std::byte *;
 
+  /// 创建一个空的帧缓冲区
   frame_buffer() : frame_buffer(0, nullptr, 0, 0) {}
 
+  /// 创建一个帧缓冲区
+  /// @param attachments_count 附件的总数目
+  /// @param attachments 所有附件的指针数组
+  /// @param width 帧缓冲区的宽度
+  /// @param height 帧缓冲区的高度
   explicit frame_buffer(
       std::uint16_t attachments_count,
       const attachment attachments[],
@@ -30,11 +38,24 @@ public:
 
   frame_buffer &operator=(frame_buffer &&);
 
-  [[nodiscard]] inline std::uint32_t width() { return m_width; }
+  /// 获取帧缓冲区的宽度
+  /// @return 当前缓冲区的宽度
+  [[nodiscard]] inline std::uint32_t width() noexcept {
+    return m_width;
+  }
 
-  [[nodiscard]] inline std::uint32_t height() { return m_height; }
+  /// 获取帧缓冲区的高度
+  /// @return 当前缓冲区的高度
+  [[nodiscard]] inline std::uint32_t height() noexcept {
+    return m_height;
+  }
 
-  [[nodiscard]] inline std::byte *attachement(std::uint8_t id) { return m_attachments[id]; }
+  /// 获取指定 ID 附件对应的指针
+  /// @param id 附件 ID
+  /// @return 附件内存指针
+  [[nodiscard]] inline attachment attachement(std::uint8_t id) {
+    return m_attachments[id];
+  }
 
 private:
 
