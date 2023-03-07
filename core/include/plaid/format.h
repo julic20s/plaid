9c40443692e0a460141d5a2d53dd6c07afb5c4b3
julic20s <julic20s@outlook.com>
2023-03-07 17:26:45 +0800

    Add comments

--- core/include/plaid/format.h
+++ core/include/plaid/format.h
@@ -5,6 +5,8 @@
 #include <cstdint>
 
 namespace plaid {
+
+// 标记像素格式，采用位掩码形式表示各个类型
 enum class format : std::uint16_t {
   undefined = 0x000,
   RG32f = 0x024,
@@ -14,19 +16,22 @@ enum class format : std::uint16_t {
   BGRA8u = 0x141,
 };
 
+/// 获取给定格式所占的字节数量
 [[nodiscard]] inline std::uint32_t format_size(format f) {
   auto num = static_cast<std::uint16_t>(f);
   return (num & 0xf) * ((num & 0xf0) >> 4);
 }
 
+/// 判断是否浮点型
 [[nodiscard]] inline bool is_float_format(format f) {
   return (static_cast<std::uint16_t>(f) & 0xf00) == 0;
 }
 
+/// 判断是否无符号整型
 [[nodiscard]] inline bool is_unsigned_integer_format(format f) {
   return (static_cast<std::uint16_t>(f) & 0xf00) == 0x100;
 }
 
-}
+} // namespace plaid
 
 #endif
