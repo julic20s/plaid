9a3bd8ca25d3d3995c24c1f239cc5d6c2e1f338e
julic20s <julic20s@outlook.com>
2023-03-17 20:13:27 +0800

    Improve style

--- json/src/value.cpp
+++ json/src/value.cpp
@@ -5,24 +5,19 @@ using namespace json;
 dom::value dom::s_null_value;
 
 const dom::value &dom::value::operator[](const char *key) const noexcept {
-  if (m_type != type::object) {
-    return s_null_value;
-  }
-  if (!m_extra) {
-    return s_null_value;
-  }
-  std::int32_t l = 0, r = m_extra - 1;
-  while (l <= r) {
-    auto mid = (l + r) >> 1;
-    auto mk = m_object[mid].key_pointer();
-    auto cmp = std::strcmp(key, mk);
-    if (!cmp) {
-      return m_object[mid].value;
-    }
-    if (cmp > 0) {
-      l = mid + 1;
-    } else {
-      r = mid - 1;
+  if (m_type == type::object) {
+    std::int64_t l = 0, r = m_extra - 1;
+    while (l <= r) {
+      auto mid = (l + r) >> 1;
+      auto cmp = std::strcmp(key, m_object[mid].key_pointer());
+      if (!cmp) {
+        return m_object[mid].value;
+      }
+      if (cmp > 0) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
     }
   }
   return s_null_value;
